50db31da4bd4293541a30dea2f69199b91f29a56
UtkuBoraEfil <utqbora@hotmail.com>
2024-08-19 17:05:29 +0300

    responsive2

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -9,13 +9,11 @@ import { LogoutButton } from "./ui/LogoutButton";
 
 export default function Navbar () { 
   return (
-    <div className="bg-light-gray rounded-r-lg  shadow-lg  pt-5 flex flex-col justify-between pl-3 z-10">
-      <div className="flex flex-col gap-5 pr-3">
-        <div className="flex justify-between mb-10">
+    <div className="bg-light-gray rounded-r-lg  shadow-lg  pt-5 flex flex-col justify-between pl-2 lg:pl-3 z-10">
+      <div className="flex flex-col gap-5 pr-2 lg:pr-3">
+        <div className="flex justify-between mb-10 gap-1">
           <Logo/>
-          <div className="hidden lg:block ">
-            <LogoutButton/>
-          </div>
+          <LogoutButton/>
         </div>
         <Favorites/>
         <History/>
